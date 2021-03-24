e90e1c233ef96507c022bc9e16270443c1ce3ed7
Matias Moll <moll.matias.a@gmail.com>
2021-03-24 12:21:54 -0300

    Ejercicio 1 y creacion de componentes ej 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ejercicios';
+  
+  public numero1:number;
+  public numero2:number;
+  public resSuma:number;
+  public resProm:number;
+
+  calcular(){
+    this.resSuma = this.numero1 + this.numero2;
+    this.resProm = this.resSuma / 2;
+    
+  }
+  limpiar(){
+    this.numero1 = 0;
+    this.numero2 = 0;
+  }
 }
---
+++ src/app/classes/user.ts
@@ -0,0 +1,10 @@
+export class User {
+
+    public nombre:String;
+    public clave:String;
+
+    public User(name:String, pass:String){
+        this.nombre = name;
+        this.clave = pass;
+    }
+}
