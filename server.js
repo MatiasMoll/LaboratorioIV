30e277782eff9a11f783329e3ff8fbb5698b7a8d
Matias Moll <moll.matias.a@gmail.com>
2021-03-24 14:21:34 -0300

    Deploy Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const path = requiere('path');
+const path = require('path');
 
 app.use(express.static(__dirname + '/angularapp'));
 app.listen(process.env.PORT || 8080);
